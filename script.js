293c29d2118f9f2b721358249729d4815a9f7b37
abhi7724 <79439870+abhi7724@users.noreply.github.com>
2022-08-08 10:18:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-let namasteBtn = document.querySelector('button');
-namasteBtn.addEventListener('click', inputMsg);
-
-function inputMsg(){
-    let name = prompt('Enter Name of student');
-    namasteBtn.textContent = 'Roll No. 1:' + name ;
-
-}
- 
-
+console.log("Script running...")
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", ()=>)
+document.querySelector('.sidebarGo').classList.toggle('sidebarGo');
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if(document.querySelector('.sidebarGo').classList.contains('sidebarGo')){
+        document.querySelector('.ham').style.display ='inline'
+        document.querySelector('.cross').style.display ='none'
+    }
